46815dc25aee205a0c0d76a527aa5fa89984cd87
Claholla <claholla@gmail.com>
2021-06-15 21:32:14 -0500

    copied previous file data to repo

--- index.js
+++ index.js
@@ -1,11 +1,81 @@
-// TODO: Include packages needed for this application
+// Application dependency requirements
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Inquirer prompt for userResponse data
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?",
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return console.log("You must enter a project title to continue.");
+                }
+                return true;
+            },
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Enter a brief description of your project",
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return console.log("You must enter a description to continue.");
+                }
+                return true;
+            },
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Enter any installation instructions for your application",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Enter any usage instructions for your project",
+        },
+        {
+            type: "input",
+            name: "contributionreq",
+            message: "Enter any instructions for contribution to your project",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose a license for your project",
+            choices: ["BSD", "MIT", "GPL"],
+        },
+        {
+            
+        }
+    ])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Generate readme function
+const genReadme = (userResponse) => {
+    `# ${userResponse.title}
+    
+    ## Description
 
+    ${userResponse.description}
+
+    ## Table of Contents
+
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contribution](#contribution)
+    - [Credits](#credits)
+    - [License](#license)
+
+    ## Installation
+
+    ${userResponse.installation}
+
+
+    `
+};
 // TODO: Create a function to initialize app
 function init() {}
 
